cdfb6f968375499905c16844824efa2e4ad837e3
jason-nordheim <jason.nordheim@gmail.com>
2020-08-28 12:53:38 -0600

    created feed component

---
+++ src/Feed.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Feed.css'
+import StoryReel from './StoryReel'
+
+function Feed() {
+  return (
+    <div className="feed">
+    <StoryReel /> 
+      {/* story row */}
+      {/* message sender  */}
+    </div>
+  )
+}
+
+
+export default Feed
