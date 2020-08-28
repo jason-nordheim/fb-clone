88841d35ed6a7c6da1bd4c14cf3b67da712a49f7
jason-nordheim <jason.nordheim@gmail.com>
2020-08-28 10:54:02 -0600

    added header component to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     <div className="app">
       <h1>Lets build a facebook clone!</h1>
 
-      {/* header */}
+      <Header /> 
 
       {/* 
         app body 
