09e15df07cba1d7500a877c217e5b54a804761f2
jason-nordheim <jason.nordheim@gmail.com>
2020-08-28 12:54:02 -0600

    created StoryReel component

---
+++ src/StoryReel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './StoryReel.css'
+import Story from './Story'
+
+function StoryReel() {
+  return (
+    <div className="storyReel">
+      <Story /> 
+    </div>
+  )
+}
+
+export default StoryReel
