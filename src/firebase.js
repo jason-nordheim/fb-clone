8c0c6f0c9f5fd5a3e2ed5a1b80c759998117854c
jason-nordheim <jason.nordheim@gmail.com>
2020-08-27 22:17:47 -0600

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDNKRyLvyQzzCwOa9HqbWUSQZJ6tAYnrlM",
+  authDomain: "fb-clone-alpha.firebaseapp.com",
+  databaseURL: "https://fb-clone-alpha.firebaseio.com",
+  projectId: "fb-clone-alpha",
+  storageBucket: "fb-clone-alpha.appspot.com",
+  messagingSenderId: "999427561555",
+  appId: "1:999427561555:web:56209e3d8a5d8b4bc0cea6",
+  measurementId: "G-GF5WT15JKC",
+};
