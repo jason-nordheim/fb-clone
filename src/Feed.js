673a8711dbf63dcaf5f5744943d2ea8d48fc8826
jason-nordheim <jason.nordheim@gmail.com>
2020-08-29 11:28:25 -0600

    added Post component with dummy data for visualization

--- src/Feed.js
+++ src/Feed.js
@@ -2,12 +2,28 @@ import React from 'react'
 import './Feed.css'
 import StoryReel from './StoryReel'
 import MessageSender from "./MessageSender";
+import Post from './Post'
 
 function Feed() {
   return (
     <div className="feed">
       <StoryReel />
       <MessageSender />
+      <Post
+        profilePic="https://avatars2.githubusercontent.com/u/24712956?s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
+        message="Wow! This works!"
+        timestamp="This is a timestamp"
+        username="ssssangha"
+        image="https://specials-images.forbesimg.com/imageserve/5f302109ffad89f9130e07db/960x0.jpg?cropX1=0&cropX2=4800&cropY1=243&cropY2=2943"
+      />
+      <Post
+        profilePic="https://avatars2.githubusercontent.com/u/24712956?s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
+        message="Wow! This works!"
+        timestamp="This is a timestamp"
+        username="ssssangha"
+      />
+      <Post />
+      <Post />
       {/* message sender  */}
     </div>
   );
