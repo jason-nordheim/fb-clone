edb60f5bbc521ee3b8444fd5126e1d0cc9859f9c
jason-nordheim <jason.nordheim@gmail.com>
2020-08-29 10:44:42 -0600

    created and styled MessageSender component

---
+++ src/MessageSender.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import "./MessageSender.css";
+import { Avatar } from "@material-ui/core";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+
+function MessageSender() {
+  const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // todo - some db stuff 
+
+    // reset input values
+    setInput('')
+    setImageUrl('')
+  }
+
+  return (
+    <div className="messageSender">
+      <div className="messageSender__top">
+        <Avatar />
+        <form>
+          <input
+            className="messageSender__input"
+            placeholder="What's on your mind?"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <input
+            placeholder="image URL (optional)"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+          />
+          <button onClick={handleSubmit} type="submit">
+            hidden submit
+          </button>
+        </form>
+      </div>
+      <div className="messageSender__bottom">
+        <div className="messageSender__option">
+          <VideocamIcon style={{ color: "red" }} />
+          <h4>Live Video</h4>
+        </div>
+        <div className="messageSender__option">
+          <PhotoLibraryIcon style={{ color: "green" }} />
+          <h4>Photo/Video</h4>
+        </div>
+        <div className="messageSender__option">
+          <InsertEmoticonIcon style={{ color: "orange" }} />
+          <h4>Feeling/Activity</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MessageSender;
