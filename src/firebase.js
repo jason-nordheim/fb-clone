feb648d6bbee358c140f4d33332c8b575a851078
jason-nordheim <jason.nordheim@gmail.com>
2020-08-30 12:57:26 -0600

    setup firebase with google Auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
   apiKey: "AIzaSyDNKRyLvyQzzCwOa9HqbWUSQZJ6tAYnrlM",
   authDomain: "fb-clone-alpha.firebaseapp.com",
@@ -8,3 +10,14 @@ const firebaseConfig = {
   appId: "1:999427561555:web:56209e3d8a5d8b4bc0cea6",
   measurementId: "G-GF5WT15JKC",
 };
+
+/* Intialize firebase app with configuration */
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+
+/* Google Authentication Provider  */
+const provider = new firebase.auth.GoogleAuthProvider() 
+
+export { auth, provider, db }
+export default db 
