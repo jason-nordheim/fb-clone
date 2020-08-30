b753b20015d39f7a35d99383f72ced811606945f
jason-nordheim <jason.nordheim@gmail.com>
2020-08-30 13:00:53 -0600

    added dispatch for setting the user upon successful login to global state (context)

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './Login.css'
 import { Button } from '@material-ui/core'
+import { auth, provider } from './firebase'
+import { useStateValue } from './StateProvider'
+import { actionTypes } from './reducer'
 
 function Login() {
+  const [state, dispatch] = useStateValue(); 
 
-  const signIn = e => {
-    // todo: sign in 
+  function signIn(e){
+    e.preventDefault() 
+    auth.signInWithPopup(provider)
+      .then(res => dispatch({type: actionTypes.SET_USER, user: res.user}))
+      .catch(err => alert(err.message))
   } 
   
   return (
@@ -15,7 +22,7 @@ function Login() {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png"
           alt="facebook-logo"
         />
-        <h3>facebook</h3>
+        <h2>facebook</h2>
       </div>
       <Button type="submit" onClick={signIn}>
         Sign In
