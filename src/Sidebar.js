fa0242ba5b374d6fabd2ded2486b177b0a84c04a
jason-nordheim <jason.nordheim@gmail.com>
2020-08-30 13:33:00 -0600

    added in user information for Sidebar component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -7,11 +7,13 @@ import StoreFrontIcon from '@material-ui/icons/Storefront'
 import VideoLibraryIcon from '@material-ui/icons/VideoLibrary'
 import ExpandMoreOutlined from "@material-ui/icons/ExpandMoreOutlined";
 import './Sidebar.css'
+import { useStateValue } from './StateProvider'
 
 function Sidebar() {
+  const [{user}, dispatch] = useStateValue() 
   return (
     <div className="sidebar">
-      <SidebarRow src='' title="Jason Nordheim" />
+      <SidebarRow src={user.photoURL} title={user.displayName} />
       <SidebarRow Icon={EmojiFlagIcon} title="Pages" />
       <SidebarRow Icon={PeopleIcon} title="Friends" />
       <SidebarRow Icon={ChatIcon} title="Messenger" />
