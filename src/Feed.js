de63fabe5d6899215102f0c9b48ae9673bfc062a
jason-nordheim <jason.nordheim@gmail.com>
2020-08-30 13:33:26 -0600

    reconfigured to pull posts from the database

--- src/Feed.js
+++ src/Feed.js
@@ -1,30 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Feed.css'
 import StoryReel from './StoryReel'
 import MessageSender from "./MessageSender";
 import Post from './Post'
+import db from './firebase'
 
 function Feed() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => {
+      setPosts(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })))
+    })
+  }, [])
+
   return (
     <div className="feed">
       <StoryReel />
       <MessageSender />
-      <Post
-        profilePic="https://avatars2.githubusercontent.com/u/24712956?s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
-        message="Wow! This works!"
-        timestamp="This is a timestamp"
-        username="ssssangha"
-        image="https://specials-images.forbesimg.com/imageserve/5f302109ffad89f9130e07db/960x0.jpg?cropX1=0&cropX2=4800&cropY1=243&cropY2=2943"
-      />
-      <Post
-        profilePic="https://avatars2.githubusercontent.com/u/24712956?s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
-        message="Wow! This works!"
-        timestamp="This is a timestamp"
-        username="ssssangha"
-      />
-      <Post />
-      <Post />
-      {/* message sender  */}
+      { posts.map(post => (
+          <Post key={post.id} 
+            image={post.data.image} 
+            timestamp={post.data.timestamp} 
+            message={post.data.message} 
+            profilePic={post.data.profilePic} 
+            username={post.data.username} 
+          /> 
+      ))}
     </div>
   );
 }
